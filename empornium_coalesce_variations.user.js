d62688091d19c066b4f6e453e4a94fc0e1cdc014
ehfraim <34144231+ehfraim@users.noreply.github.com>
2018-02-01 20:50:07 +0100

    Updates version number

--- empornium_coalesce_variations.user.js
+++ empornium_coalesce_variations.user.js
@@ -6,7 +6,7 @@
 // @exclude        /https?://www\.empornium\.(me|sx)/torrents\.php\?id=/
 // @exclude        /https?://www\.empornium\.(me|sx)/torrents\.php\?action=notify/
 // @grant          none
-// @version         1
+// @version         2
 // ==/UserScript==
 
 const variationRegexes = [
