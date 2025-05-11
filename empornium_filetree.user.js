575328ed4e16899755cfceec60ca35c0de735a55
ehfraim <34144231+ehfraim@users.noreply.github.com>
2025-05-11 17:38:02 +0200

    inject-into page for violentmonkey

--- empornium_filetree.user.js
+++ empornium_filetree.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         empornium better filelist
-// @version      3.3
+// @version      3.3.1
 // @description  Shows filelist as expandable tree structure
 // @author       ephraim
 // @namespace    empornium
@@ -8,6 +8,7 @@
 // @match        https://www.empornium.me/torrents.php?id=*
 // @match        https://www.empornium.sx/torrents.php?id=*
 // @grant        none
+// @inject-into  page
 // @downloadURL  https://update.sleazyfork.org/scripts/433858/empornium%20better%20filelist.user.js
 // @updateURL    https://update.sleazyfork.org/scripts/433858/empornium%20better%20filelist.user.js
 // ==/UserScript==
